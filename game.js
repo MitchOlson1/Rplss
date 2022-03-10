c2a204127ee8ff719eb5e9a411bf69161c418251
varNum462 <gary@varnumweb.com>
2022-03-09 19:40:13 -0500

    Finished 2-player version basic code, working to show winner best 2 out of three.

--- game.js
+++ game.js
@@ -36,12 +36,13 @@ class Game {
         if (gameType == 1){
             this.Player2 = new players.AI("Player 2 (AI)", 0);
             console.log(`Player1 = ${this.Player1.name} and Player2 = ${this.Player2.name}`);
-            this.chooseGesture();
+            this.chooseGesture("Player1",gameType);
             
         }else if(gameType == 2){
+            this.Player1 = new players.Human("Player 1", 0);
             this.Player2 = new players.Human("Player 2", 0);
             console.log(`Player1 = ${this.Player1.name} and Player2 = ${this.Player2.name}`);
-            this.chooseGesture();
+            this.chooseGesture("Player1",gameType);
         }
         else{
             console.log("Invalid choice")
@@ -53,28 +54,105 @@ class Game {
         let gestures = [Rock, Paper, Scissors, Lizard, Spock];
     }
     
-    gameRules(){
-        console.log(`Rock crushes Scissors  
-        Scissors cuts Paper  
-        Paper covers Rock   
-        Rock crushes Lizard   
-        Lizard poisons Spock   
-        Spock smashes Scissors  
-        Scissors decapitates Lizard  
-        Lizard eats Paper   
-        Paper disproves Spock   
-        Spock vaporizes Rock`);
+    aiGesture(){
+
     }
 
-    chooseGesture(){
-        console.log(`Player 1, choose a gesture - Rock, Paper, Scissors, Lizard, or Spock?`);
+    // TODO - pass player name to chooseGesture, add logic for player 2 human or AI
+    chooseGesture(whichPlayer,gameType){
+        console.log(`${whichPlayer}, choose a gesture - Rock, Paper, Scissors, Lizard, or Spock?`);
         let gestureChoice = prompt();
-        this.Player1.gesture = gestureChoice;
-        console.log(`You chose ${this.Player1.gesture}`);
+        if(whichPlayer === "Player1"){
+            this.Player1.gesture = gestureChoice;
+            //console.log(`${this.Player1.gesture}`);
+            this.chooseGesture("Player2",gameType);
+        }
+        else if(whichPlayer === "Player2"){
+            if(gameType == 2){
+                this.Player2.gesture = gestureChoice;
+                console.log(`${this.Player1.gesture} vs ${this.Player2.gesture}`);
+                this.gameProgress(gameType);
+            }else{
+                this.aiGesture();
+            }
+        };
+        
+    }
+
+    player1Wins(gameType){
+        this.Player1.wins += 1;
+        if (this.Player1.wins === 2){
+            console.log(`Congrats! Player 1 wins the game!`);
+        }else{
+            console.log(`Player 1 wins this round. Go again! Score is ${this.Player1.wins} to ${this.Player2.wins}, Player 1's favor.`);
+            this.chooseGesture("Player1",gameType);
+        };
+    }
+
+    // TOD - Change win message to "You got beat by AI!" if computer wins
+    player2Wins(gameType){
+        this.Player2.wins += 1;
+        if (this.Player2.wins === 2){
+            console.log(`Congrats! Player 2 wins the game!`);
+        }else{
+            console.log(`Player 2 wins this round. Go again! Score is ${this.Player2.wins} to ${this.Player2.wins}, Player 2's favor.`);
+            this.chooseGesture("Player1",gameType);
+        };
     }
     
-    gameProgress(){
-        
+    gameProgress(gameType){
+        let player1Choice = this.Player1.gesture;
+        let player2Choice = this.Player2.gesture;
+        if(player1Choice ===  player2Choice){
+            console.log(`It's a tie! Go again!`);
+            this.chooseGesture("Player1",gameType);
+        }
+        else if (player1Choice === "Rock"){
+            if(player2Choice === "Scissors" || player2Choice === "Lizard"){
+                this.player1Wins(gameType);
+            }
+            else if (player2Choice === "Paper" || player2Choice === "Spock")
+            {
+                this.player2Wins(gameType);
+            }
+        }        
+        else if (player1Choice === "Scissors"){
+            if(player2Choice === "Paper" || player2Choice === "Lizard"){
+                this.player1Wins(gameType);
+            }
+            else if (player2Choice === "Rock" || player2Choice === "Spock")
+            {
+                this.player2Wins(gameType);
+            }
+        }        
+        else if (player1Choice === "Paper"){
+            if(player2Choice === "Rock" || player2Choice === "Spock"){
+                this.player1Wins(gameType);
+            }
+            else if (player2Choice === "Scissors" || player2Choice === "Lizard")
+            {
+                this.player2Wins(gameType);
+            }
+        }        
+        else if (player1Choice === "Lizard"){
+            if(player2Choice === "Paper" || player2Choice === "Spock"){
+                this.player1Wins(gameType);
+            }
+            else if (player2Choice === "Scissors" || player2Choice === "Rock")
+            {
+                this.player2Wins(gameType);
+            }
+        }        
+        else if (player1Choice === "Spock"){
+            if(player2Choice === "Scissors" || player2Choice === "Rock"){
+                this.player1Wins(gameType);
+            }
+            else if (player2Choice === "Lizard" || player2Choice === "Paper")
+            {
+                this.player2Wins(gameType);
+            }
+        }
+       //gesture logic - compare choices
     }   
     
     /*
